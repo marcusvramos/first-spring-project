c99b73ff10687429914c2f8fb11d547e8e96336e
marcus-rubcube <marcus.vinicius@rubcube.com>
2024-03-08 09:59:09 -0300

    feat: starting spring

--- src/main/java/org/example/firstsprintproject/FirstSprintProjectApplication.java
+++ src/main/java/org/example/firstsprintproject/FirstSprintProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstSprintProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSprintProjectApplication.class, args);
 	}
-
 }
--- src/main/java/org/example/firstsprintproject/controller/HelloRestController.java
+++ src/main/java/org/example/firstsprintproject/controller/HelloRestController.java
@@ -1,4 +1,21 @@
 package org.example.firstsprintproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
 public class HelloRestController {
+    @GetMapping("/")
+    public String helloSpring() {
+        return "Hello Spring!";
+    }
+
+    @GetMapping("/random")
+    public String randomMessage() {
+        String[] mens={"Alo!", "Oi","Blz?","E ai?", "Jogos do dia"};
+        return mens[new Random().nextInt(mens.length)];
+    }
 }
+
